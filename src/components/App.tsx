6477a117e5e0ee6537ff9229a537ad3299174d03
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-02-17 14:36:54 +0100

    feat: TicTacToeInput.css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,15 @@ import './App.css';
 import TicTacToeInput from './TicTacToeInput/TicTacToeInput';
 
 class App extends Component {
+  constructor(props){
+    super (props)
+
+    this.state={
+      PLAYER_ONE_SYMBOL: "X",
+      PLAYER_TWO_SYMBOL: "0",
+      
+    }
+  }
   render() {
     return (
       <div className="App">
