cbb9a72bc956458ff4f3a6eef41b9a4568300008
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-02-17 13:46:33 +0100

    feat: add TicTacToeInput.tsx + test.tsx, delete logo import from App.tsx

---
+++ src/components/TicTacToeInput/TicTacToeInput.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './TicTacToeInput.css';
+
+class TicTacToeInput extends Component {
+  render() {
+    return (
+      <div className="TicTacToeInput">
+       ToDo
+      </div>
+    );
+  }
+}
+
+export default TicTacToeInput;
