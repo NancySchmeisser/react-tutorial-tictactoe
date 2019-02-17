eee0e19258fc81e53f3bb0867f84b9d3d82cf4ea
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-02-17 14:45:49 +0100

    feat: add App.css, app.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,22 +3,70 @@ import './App.css';
 import TicTacToeInput from './TicTacToeInput/TicTacToeInput';
 
 class App extends Component {
-  constructor(props){
+  constructor(props:{}){
     super (props)
 
     this.state={
       PLAYER_ONE_SYMBOL: "X",
       PLAYER_TWO_SYMBOL: "0",
+      currentTurn: "X",
+      board: [
+        "", "", "", "", "", "", "", "", "",
+      ],
+      winner: null,
       
     }
   }
-  render() {
-    return (
-      <div className="App">
-       <TicTacToeInput/>
-      </div>
-    );
+  handleClick(index) {
+    if(this.state.board[index] === "" && !this.state.winner) {
+      this.state.board[index] = this.state.currentTurn
+      this.setState({
+        board: this.state.board,
+        currentTurn: this.state.currentTurn === this.state.PLAYER_ONE_SYMBOL ? this.state.PLAYER_TWO_SYMBOL : this.state.PLAYER_ONE_SYMBOL,
+        winner: this.checkForWinner(),
+      })
+    }
   }
+
+  checkForWinner() {
+    var currentTurn = this.state.currentTurn
+    var symbols = this.state.board
+    var winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    return winningCombos.find(function(combo) {
+      if(symbols[combo[0]] !== "" && symbols[combo[1]] !== ""  && symbols[combo[2]] !== ""  && symbols[combo[0]] === symbols[combo[1]] && symbols[combo[1]] === symbols[combo[2]]) {
+        return currentTurn
+      } else {
+        return false
+      }
+    })
 }
+render() {
+  return (
+    <div className="app-container">
+      {this.state.winner ? <h1>{`The winner is ${this.state.winner}`}</h1> : null}
+      <div className="board">
+      {this.state.board.map((cell, index) => {
+        return <div onClick={() => this.handleClick(index)} className="square">{cell}</div>;
+      })}
+      </div>
+    </div>
+  )
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
