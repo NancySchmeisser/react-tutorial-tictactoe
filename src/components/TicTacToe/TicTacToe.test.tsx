7f10a16d19786cd53b8f8c1cbce93366ebf242a1
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-02-18 10:26:36 +0100

    feat: TicTacToe

---
+++ src/components/TicTacToe/TicTacToe.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TicTacToe from './TicTacToe';
+import {mount} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<TicTacToe state='X'/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
---
+++ src/components/TicTacToe/TicTacToe.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+interface TicTacToeProps {
+    
+}
+
+class TicTacToe extends Component <TicTacToeProps> {
+    render() {
+        return (
+            
+            <div className="TicTacToe">
+              
+            </div>
+            
+        );
+    }
+}
+
+export default TicTacToe;
